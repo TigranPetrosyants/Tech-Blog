da0dd24fb72132ffc6b8c15e222e9fccb4d658e4
TigranPetrosyants <tigran.petrosyants.fd@gmail.com>
2023-04-14 13:36:02 +0400

    Add firebase

---
+++ src/app/components/pages-list/pages-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'pages-list',
+  templateUrl: './pages-list.component.html',
+  styleUrls: ['./pages-list.component.css']
+})
+export class PagesListComponent implements OnInit {
+  pages$: Observable<any[]>;
+
+  constructor(
+    private db: AngularFireDatabase,
+    ) {  }
+  
+  ngOnInit(): void {
+    this.pages$ = this.db.list('pages').valueChanges()
+  }
+
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDiyVHBo88DwZAVKX4LN_VvVxwoexP1J74",
+        authDomain: "blog-8b1cd.firebaseapp.com",
+        projectId: "blog-8b1cd",
+        storageBucket: "blog-8b1cd.appspot.com",
+        messagingSenderId: "1080215563130",
+        appId: "1:1080215563130:web:f3a489eeb7fd22187244c3",
+        measurementId: "G-TWVBR1EP68" 
+    }
+};
