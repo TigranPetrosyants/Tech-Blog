e30909f9981347a873d7dec1598f2e814a97c63d
TigranPetrosyants <tigran.petrosyants.fd@gmail.com>
2023-04-17 19:19:23 +0400

    Add posts service

--- src/app/services/posts/post.service.ts
+++ src/app/services/posts/post.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs';
+import { Post } from 'src/app/providers/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore
+  ) { }
+
+  getPosts() {
+    return this.afs.collection("posts").snapshotChanges().pipe(
+      map(post => {
+        return post.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;          
+          return {id, ...data}
+        })
+      })
+    );
+  }
+
+  addPost(post: Post): void {
+    this.afs.collection("posts").add(post);
+  }
+
+  deletePost(postId: string): void {
+    this.afs.doc(`posts/${postId}`).delete();
+  }
+
+  updatePost(postId: string, post: Post): void {
+    this.afs.doc(`posts/${postId}`).update(post);
+  }
 }
